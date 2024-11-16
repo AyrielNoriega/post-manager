cef7de5e05f57ecf3f75a08425a330aafe2fbc2a
AyrielNoriega <anoriega2310@gmail.com>
2024-11-16 17:54:23 -0500

    Implement CRUD functionality for posts with API routes and database seeding Add test for CRUD

---
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Models\Post;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+            $posts = Post::all();
+            return response()->json($posts);
+        } catch (\Exception $e) {
+            Log::error("An error occurred: {$e->getMessage()}");
+            return response()->json(['message' => 'An error occurred'], 500);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+    {
+        try {
+            $post = Post::create($request->all());
+            return response()->json($post, 201);
+        } catch (\Exception $e) {
+            Log::error("An error occurred: {$e->getMessage()}");
+            return response()->json(['message' => 'An error occurred'], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+            return $post;
+        } catch (ModelNotFoundException $e) {
+            Log::error("Post not found: {$e->getMessage()}");
+            return response()->json(['message' => 'Post not found'], 404);
+        } catch (\Exception $e) {
+            Log::error("An error occurred: {$e->getMessage()}");
+            return response()->json(['message' => 'An error occurred'], 500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePostRequest $request, string $id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+            $post->update($request->all());
+            return response()->json($post);
+        } catch (ModelNotFoundException $e) {
+            Log::error("Post not found: {$e->getMessage()}");
+            return response()->json(['message' => 'Post not found'], 404);
+        } catch (\Exception $e) {
+            Log::error("An error occurred: {$e->getMessage()}");
+            return response()->json(['message' => 'An error occurred'], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+            $post->delete();
+            return response()->json(null, 204);
+        } catch (ModelNotFoundException $e) {
+            Log::error("Post not found: {$e->getMessage()}");
+            return response()->json(['message' => 'Post not found'], 404);
+        } catch (\Exception $e) {
+            Log::error("An error occurred: {$e->getMessage()}");
+            return response()->json(['message' => 'An error occurred'], 500);
+        }
+    }
+}
---
+++ tests/Feature/Http/Controllers/V1/PostControllerTest.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\V1;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Prueba para mostrar todos los posts.
+     * @return void
+     */
+    public function test_all_posts(): void
+    {
+        User::factory()->create();
+        Post::factory()->count(5)->create();
+        $response = $this->getJson('/api/v1/posts');
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonCount(5);
+    }
+
+
+    /**
+     * Prueba para crear un post.
+     * @return void
+     */
+    public function test_store_post(): void
+    {
+        User::factory()->create();
+        $post = Post::factory()->create();
+        $response = $this->postJson('/api/v1/posts', $post->toArray());
+        $response->assertStatus(201);
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonFragment([
+            'title' => $post['title'],
+            'content' => $post['content'],
+            'user_id' => $post['user_id'],
+        ]);
+    }
+
+    /**
+     * Prueba para validar errores al crear un post.
+     * @return void
+     *
+     */
+    public function test_store_post_validation_error(): void
+    {
+        User::factory()->create();
+        $response = $this->postJson('/api/v1/posts', []);
+        $response->assertStatus(422);
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonValidationErrors(['title', 'content', 'user_id']);
+    }
+
+
+    /**
+     * Prueba para mostrar un post.
+     * @return void
+     */
+    public function test_show_post(): void
+    {
+        User::factory()->create();
+        $post = Post::factory()->create();
+        $response = $this->getJson("/api/v1/posts/{$post->id}");
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJson($post->toArray());
+    }
+
+
+    /**
+     * Prueba para validar errores al mostrar un post que no existe.
+     * @return void
+     */
+    public function test_show_post_not_found(): void
+    {
+        User::factory()->create();
+        $response = $this->getJson('/api/v1/posts/1');
+        $response->assertStatus(404);
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonFragment([
+            'message' => 'Post not found',
+        ]);
+    }
+
+
+    /**
+     * Prueba para actualizar un post.
+     * @return void
+     */
+    public function test_update_post(): void
+    {
+        User::factory()->create();
+        $post = Post::factory()->create();
+        $response = $this->putJson("/api/v1/posts/{$post->id}", $post->toArray());
+        $response->assertStatus(200);
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonFragment([
+            'title' => $post['title'],
+            'content' => $post['content'],
+            'user_id' => $post['user_id'],
+        ]);
+    }
+
+
+    /**
+     * Prueba para validar errores al actualizar un post que no existe.
+     * @return void
+     */
+    public function test_update_post_not_found(): void
+    {
+        User::factory()->create();
+        $response = $this->putJson('/api/v1/posts/1', []);
+        $response->assertStatus(404);
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonFragment([
+            'message' => 'Post not found',
+        ]);
+    }
+
+
+    /**
+     * Prueba para validar errores al actualizar un post.
+     * @return void
+     */
+    // public function test_update_post_validation_error(): void
+    // {
+    //     User::factory()->create();
+    //     $post = Post::factory()->create();
+    //     $response = $this->putJson("/api/v1/posts/{$post->id}", ['title_error' => '', 'content' => '', 'user_id' => '']);
+    //     $response->assertStatus(422);
+    //     $response->assertHeader('content-type', 'application/json');
+    //     $response->assertJsonValidationErrors(['title', 'content', 'user_id']);
+    // }
+
+
+    /**
+     * Prueba para eliminar un post.
+     * @return void
+     */
+    public function test_delete_post(): void
+    {
+        User::factory()->create();
+        $post = Post::factory()->create();
+        $response = $this->deleteJson("/api/v1/posts/{$post->id}");
+        $response->assertStatus(204);
+    }
+
+
+    /**
+     * Prueba para validar errores al eliminar un post que no existe.
+     * @return void
+     */
+    public function test_delete_post_not_found(): void
+    {
+        User::factory()->create();
+        $response = $this->deleteJson('/api/v1/posts/1');
+        $response->assertStatus(404);
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonFragment([
+            'message' => 'Post not found',
+        ]);
+    }
+}
+
+
