9f762552308273a9b7981181d67fd1e7976afd9c
AyrielNoriega <anoriega2310@gmail.com>
2024-11-16 19:26:15 -0500

    Add unauthenticated response handling in middleware and update database seeder to include UserSeeder and PostSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'jon',
+            'email' => 'jon@gmail.com',
+            'password' => '12345678',
+        ]);
+    }
+}
